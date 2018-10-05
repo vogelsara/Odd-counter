472d8b52fb52ead1e91e9ee411436a0a50bbe676
Sára <sari.vogel.94@gmail.com>
2018-10-05 11:17:31 +0200

    Add style

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 
 
 function calculateTheOdd () {
-    var payed = parseInt(document.getElementById('payed').value);
-    var price = parseInt(document.getElementById('price').value);
+    var payed = document.getElementById('payed').value;
+    var price = document.getElementById('price').value;
     var odd = payed - price;
 
     document.getElementById('result').value = odd;
 }
+
+
 
 
